b82a0465e689a58f6cb1ac279a1b2a6a24ebf415
Lahcen Kazaz <lkazaz@c4r1p2.1337.ma>
2024-10-02 17:01:24 +0100

    commit number 572

--- cpp08/ex01/Span.cpp
+++ cpp08/ex01/Span.cpp
@@ -1,52 +1,87 @@
 #include "Span.hpp"
+#include <cmath>
+#include <climits>
 
-
-Span::Span():N(0), nbr_stored(0), array(0){
+Span::Span() : N(0), nbr_stored(0), array(0)
+{
 }
 
-Span::Span(unsigned int N){
+Span::Span(unsigned int N)
+{
     nbr_stored = 0;
     this->N = N;
-    if(array.size())
+    if (array.size())
         array.clear();
-    this->array.resize(N,1);
+    this->array.resize(N, 1);
 }
 
-Span::Span(const Span& other){
+Span::Span(const Span &other)
+{
     *this = other;
 }
 
-Span& Span::operator=(const Span& other){
-    if(this != &other)
+Span &Span::operator=(const Span &other)
+{
+    if (this != &other)
     {
-
     }
     return *this;
 }
 
-Span::~Span(){
+Span::~Span()
+{
 }
 
-void Span::addNumber(int n){
-    if(this->N > this->nbr_stored)
+void Span::addNumber(int n)
+{
+    if (this->N > this->nbr_stored)
     {
-        this->array.at(nbr_stored)=n;
+        this->array.at(nbr_stored) = n;
         this->nbr_stored++;
     }
     else
         throw std::runtime_error("Error");
 }
 
-int Span::shortestSpan(){
-    int shor=0;
-    for(size_t i=0; i<array.size();i++){
-        if(i+1<array.size() && shor>= array[i]-array[i+1])
-            shor = array[i] - array[i+1];
-    }
+int Span::shortestSpan()
+{
+    int shor = INT_MAX;
+    int max = 0;
+    int min = 0;
+    for (size_t i = 0; i < array.size(); i++)
+    {
 
+        for (size_t j = i+1; j < array.size(); j++)
+        {
+            if (i < array.size())
+            {
+                max = std::max(array[i], array[j]);
+                min = std::min(array[i], array[j]);
+                if (shor > (max - min))
+                    shor = (max - min);
+            }
+        }
+    }
     return shor;
 }
+int Span::longestSpan()
+{
+    int log = 0;
+    int max = 0;
+    int min = 0;
+    for (size_t i = 0; i < array.size(); i++)
+    {
+        for (size_t j = i+1; j < array.size(); j++)
+        {
+            if (i < array.size())
+            {
+                max = std::max(array[i], array[j]);
+                min = std::min(array[i], array[j]);
+                if (log < (max - min))
+                    log = (max - min);
+            }
+        }
+    }
 
-int Span::longestSpan(){
-    return 0;
+    return log;
 }
--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -2,18 +2,20 @@
 
 int main()
 {
-    try{
+    try
+    {
         Span sp = Span(5);
         sp.addNumber(6);
-        sp.addNumber(39999);
+        sp.addNumber(3);
         sp.addNumber(17);
         sp.addNumber(9);
-        sp.addNumber(11);
-        std::cout<<sp.shortestSpan()<<std::endl;
+        sp.addNumber(-17);
+        std::cout << sp.shortestSpan() << std::endl;
+        std::cout << sp.longestSpan() << std::endl;
     }
-    catch(std::exception &e)
+    catch (std::exception &e)
     {
-        std::cout<<e.what()<<std::endl;
+        std::cout << e.what() << std::endl;
     }
     return 0;
 }
